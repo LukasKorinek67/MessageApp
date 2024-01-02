6431870d937bc166bacd95c464eb39b46553843b
LukasKorinek67 <korinek.lukas@post.cz>
2024-01-02 22:22:47 +0100

    Firebase Admin SDK update - using env variables instead of json file

--- backend/src/main/java/com/korinek/message_app/backend/configuration/FirebaseConfiguration.java
+++ backend/src/main/java/com/korinek/message_app/backend/configuration/FirebaseConfiguration.java
@@ -4,37 +4,40 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
-import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfiguration {
 
+    private final FirebaseConfigurationResource firebaseConfigurationResource;
+
+    @Autowired
+    public FirebaseConfiguration(FirebaseConfigurationResource firebaseConfigurationResource) {
+        this.firebaseConfigurationResource = firebaseConfigurationResource;
+    }
+
     @PostConstruct
     public void initialize() throws IOException {
-        Resource resource = new ClassPathResource("firebase-admin-sdk.json");
+        String jsonContent = "";
+        try {
+            jsonContent = this.firebaseConfigurationResource.getJsonContent();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
-        resource.getInputStream();
-        try (InputStream serviceAccount = resource.getInputStream()) {
+        try (InputStream serviceAccount = new ByteArrayInputStream(jsonContent.getBytes());) {
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
-
             FirebaseApp.initializeApp(options);
         } catch(IOException e) {
             e.printStackTrace();
         }
-        /*FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase-admin-sdk.json");
-
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
-
-        FirebaseApp.initializeApp(options);*/
     }
 }
---
+++ backend/src/main/java/com/korinek/message_app/backend/configuration/FirebaseConfigurationResource.java
@@ -0,0 +1,53 @@
+package com.korinek.message_app.backend.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FirebaseConfigurationResource {
+
+    @Value("${FIREBASE_ADMIN_TYPE}")
+    String firebaseType;
+    @Value("${FIREBASE_ADMIN_PROJECT_ID}")
+    String firebaseProjectId;
+    @Value("${FIREBASE_ADMIN_PRIVATE_KEY_ID}")
+    String firebasePrivateKeyId;
+    @Value("${FIREBASE_ADMIN_PRIVATE_KEY}")
+    String firebasePrivateKey;
+    @Value("${FIREBASE_ADMIN_CLIENT_EMAIL}")
+    String firebaseClientEmail;
+    @Value("${FIREBASE_ADMIN_CLIENT_ID}")
+    String firebaseClientId;
+    @Value("${FIREBASE_ADMIN_AUTH_URI}")
+    String firebaseAuthUri;
+    @Value("${FIREBASE_ADMIN_TOKEN_URI}")
+    String firebaseTokenUri;
+    @Value("${FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL}")
+    String firebaseAuthProviderX509;
+    @Value("${FIREBASE_ADMIN_CLIENT_X509_CERT_URL}")
+    String firebaseClientX509;
+    @Value("${FIREBASE_ADMIN_UNIVERSE_DOMAIN}")
+    String firebaseUniverseDomain;
+
+    public String getJsonContent() throws JSONException {
+        String jsonContent = """
+        {
+            "type": "%s",
+            "project_id": "%s",
+            "private_key_id": "%s",
+            "private_key": %s,
+            "client_email": "%s",
+            "client_id": "%s",
+            "auth_uri": "%s",
+            "token_uri": "%s",
+            "auth_provider_x509_cert_url": "%s",
+            "client_x509_cert_url": "%s",
+            "universe_domain": "%s"
+        }
+        """.formatted(this.firebaseType, this.firebaseProjectId, this.firebasePrivateKeyId, this.firebasePrivateKey,
+                this.firebaseClientEmail, this.firebaseClientId, this.firebaseAuthUri, this.firebaseTokenUri, this.firebaseAuthProviderX509,
+                this.firebaseClientX509, this.firebaseUniverseDomain);
+        return jsonContent;
+    }
+}
