02518fc6e561edfcbbca8d951f617bb04b262786
LukasKorinek67 <korinek.lukas@post.cz>
2024-01-30 15:38:19 +0100

    Railway path name fix

--- frontend/src/components/buttons/login-buttons/LoginGoogleButton.js
+++ frontend/src/components/buttons/login-buttons/LoginGoogleButton.js
@@ -1,8 +1,8 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import {auth} from "./../../../config/firebase.js";
 import BootstrapIcons from "../../../utils/BootstrapIcons";
 import LoginButton from "./LoginButton";
 import * as strings from "../../../strings/strings.js";
+import {auth} from "./../../../config/firebase.js";
 
 
 export default function LoginGoogleButton() {
